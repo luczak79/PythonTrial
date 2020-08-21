bcb7c70d490ae58a63f04b4076ee1acd6ce1d86f
luczak79 <luczak79@googlemail.com>
2020-08-21 21:04:13 +0200

    filter for objects wirh __str__

--- functionalProgramming/functionalProgrammingFilter.py
+++ functionalProgramming/functionalProgrammingFilter.py
@@ -2,12 +2,45 @@
 #  Functional Programming - filter
 #################################
 
+class Czlowiek():
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return 'Name: {self.name}, Age: {self.age}'.format(self=self)
+
 def only_odd(item):
   return item % 2 != 0
 
-filter_list = [1,2,3,4,5,6,7,8,9]
-result = filter(only_odd, filter_list)
+def moreThanFive(number):
+  if number > 5:
+    return True
+  else:
+    return False
 
-print(type(result))
-print(list(result))
-print(filter_list)
+def tylkoPelnoletni(uczestnik):
+  if uczestnik.age > 18:
+    return True
+  else:
+    return False
+
+listaUczestnikowWycieczki = []
+listaUczestnikowWycieczki.append(Czlowiek("Marcin", 41))
+listaUczestnikowWycieczki.append(Czlowiek("Janek", 43))
+listaUczestnikowWycieczki.append(Czlowiek("Julka", 17))
+listaUczestnikowWycieczki.append(Czlowiek("Tomek", 33))
+listaUczestnikowWycieczki.append(Czlowiek("Ewelina", 16))
+
+filter_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+result1 = filter(only_odd, filter_list)
+result2 = filter(moreThanFive, filter_list)
+pelnoletni = list(filter(tylkoPelnoletni, listaUczestnikowWycieczki))
+
+print('orginalna lista:',filter_list)
+print('nieparzyste:',list(result1))
+print('wieksze niż 5:',list(result2))
+
+print('Pelnoletni uczestnicy wycieczki:')
+for x in pelnoletni:
+  print(x.__str__())
